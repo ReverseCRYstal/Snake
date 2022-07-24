8dec9d621623e9aae0a29b475f97067efed89182
TheCryStal <109683914+ReverseCRYstal@users.noreply.github.com>
2022-07-25 05:06:03 +0800

    more elegant

--- src/main.cpp
+++ src/main.cpp
@@ -1,146 +1,169 @@
-// Length of Field, you can also use the constant instead, whatever, they are the same
-#define FIELD_LENGTH 15
-// Width of Field
-#define FIELD_WIDTH 15
-#define _USE_UGLY_ARRAY
+#include "compati_stuff.h"
 
-#ifdef _MSC_VER
-#define _CRT_SECURE_NO_WARNINGS
-#endif // _MSC_VER
+#include <cstdio>
+#include <ctime>
+#include <cstdlib>
+#include <list>
+#include <thread>
+#include <clocale>
 
+// four kind of direction
+enum class dir4 :unsigned {
+	forward = 0u,
+	backward,
+	left_toward,
+	right_toward
+};
 
-#include "snake.h"
+enum class element :unsigned char {
+	blank = 0u,
+	head,
+	body,
+	food
+};
+
+// width of field
+constexpr unsigned char width = 15;
+// length of field
+constexpr unsigned char length = 15;
+
+constexpr position spawnPoint = { length / 2 + length % 2,width / 2 + width % 2 };
+
+// Array is kind of Hash so it is high-effective
+constexpr dir4 dirOppositeImg[4] = {
+	dir4::backward,
+	dir4::forward,
+	dir4::right_toward,
+	dir4::left_toward
+};
+
+// game field, all gaming data will record on it
+element field[length][width] = { {} };
+
+unsigned score = 0u;
+
+void Move(position& pos, dir4 dir) {
+	switch (dir)
+	{
+	case dir4::forward:
+		pos.y--;
+		if (pos.y < 0)
+			pos.y = length - 1;
+		break;
+	case dir4::backward:
+		pos.y++;
+		if (pos.y > length - 1)
+			pos.y = 0;
+		break;
+	case dir4::left_toward:
+		pos.x--;
+		if (pos.x < 0)
+			pos.x = width - 1;
+		break;
+	case dir4::right_toward:
+		pos.x++;
+		if (pos.x > width - 1)
+			pos.x = 0;
+		break;
+	}
+}
+
+void PrintField() {
+	for (size_t i = 0; i < length; i++) {
+		for (size_t j = 0; j < width; j++)
+			putwchar(L"¡õ¡ñ¡ö¡î"[unsigned(field[i][j])]);
+		putchar('\n');
+	}
+	printf("current score: %d", score);
+}
 
 int main() {
-	// initialization
+	setlocale(LC_CTYPE, "");
 	srand(unsigned(time(NULL)));
 
-	setlocale(LC_ALL, "");
+	std::list<dir4> dirQuque;
 
-	for (int i = 0; i < FIELD_LENGTH; i++)
-		memset((void*)field[i], blank, FIELD_WIDTH);
+	dir4 curDir, dirRec;
+	curDir = dirRec = dir4::forward;
 
-	SnakeNode snake[FIELD_LENGTH * FIELD_WIDTH];
+	position foodPos = { rand() % length,rand() % width };
+	position tail = spawnPoint, head = spawnPoint;
 
-	Position* tmp = new Position(
-		{
-			FIELD_LENGTH / 2 + FIELD_LENGTH % 2 - 1 ,
-			FIELD_WIDTH / 2 + FIELD_WIDTH % 2 - 1
-		});
+	field[foodPos.y][foodPos.x] = element::food;
 
-	NodeInit(snake, *tmp, FORWARD);
-
-	field[tmp->x][tmp->y] = head;
-
-	// got char
-	int ch;
-	// current Direction of snake head
-	int currentDir = FORWARD;
 	int snakeLength = 1;
-	// food generate
-	Position foodPos = { rand() % FIELD_WIDTH,rand() % FIELD_LENGTH };
-	field[foodPos.y][foodPos.x] = food;
-	int score = 0;
-	unsigned pauseTime = 200;
+	unsigned pauseTime = 100u;
 
-	delete tmp;
+	char got = '\0';
+
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
 
 	// main loop
 	while (true) {
+		if (keyboard_hit()) {
 
-		// display
-		FieldPrint(field);
-		cout << "score: " << score << endl;
-		if (_kbhit()) {
-			ch = _getch();
-			switch (ch) {
+			switch (getchar_no_echo()) {
 			case 'w':
 			case 'W':
-				currentDir = FORWARD;
+				dirRec = dir4::forward;
 				break;
 			case 'a':
 			case 'A':
-				currentDir = LEFT_TOWARD;
+				dirRec = dir4::left_toward;
 				break;
 			case 's':
 			case 'S':
-				currentDir = BACKWARD;
+				dirRec = dir4::backward;
 				break;
 			case 'd':
 			case 'D':
-				currentDir = RIGHT_TOWARD;
+				dirRec = dir4::right_toward;
 				break;
 			default:
 				goto out_of_chk;
 				break;
 			}
-			if (currentDir == snake->direction)
-				pauseTime = 40;
-			else if (snake->direction == directionsImage[currentDir] && snakeLength == 1)
-				snake->direction = currentDir;
-			else if (snake->direction != directionsImage[currentDir])
-				snake->direction = currentDir;
-
+			if (snakeLength == 1 && dirOppositeImg[(size_t)dirRec] == curDir ||
+				dirOppositeImg[size_t(dirRec)] != curDir)
+				curDir = dirRec;
+			else if (dirRec == curDir)
+				pauseTime = pauseTime / 2;
 		}
-
 	out_of_chk:
-		SnakeNode tail = snake[snakeLength - 1];
+		dirQuque.push_back(curDir);
 
-		Position before = snake->nodePos;
-		Move(snake->direction, &snake->nodePos);
-		Position after = snake->nodePos;
-
-		switch (field[snake->nodePos.y][snake->nodePos.x])
+		position before = head;
+		// move
+		Move(head, curDir);
+		switch (field[head.y][head.x])
 		{
-		case blank:
-			snake->nodePos = before;
-
-			// 'i' can be size_t
-			for (int i = snakeLength - 2, j = snakeLength - 1; i >= 0; i--, j--)
-				snake[j] = snake[i];
-
-			snake->nodePos = after;
-			field[before.y][before.x] = body;
-			field[tail.nodePos.y][tail.nodePos.x] = blank;
-			field[snake->nodePos.y][snake->nodePos.x] = head;
+		case element::blank:
+			field[head.y][head.x] = element::head;
+			field[before.y][before.x] = element::body;
+			field[tail.y][tail.x] = element::blank;
+			Move(tail, dirQuque.front());
+			dirQuque.pop_front();
 			break;
-		case food:
-			snake[snakeLength] = tail;
+		case element::body:
+			clear_screen_no_sparkle(consoleHandle);
+			puts("GAME OVER!\n");
+			printf("Finally score: %d", score);
+			system("pause");
+			exit(0);
+		case element::food:
+			field[before.y][before.x] = element::body;
+			field[head.y][head.x] = element::head;
 			snakeLength++;
 			score += 100;
-			snake->nodePos = before;
-
-			// 'i' can be size_t
-			for (int i = snakeLength - 2, j = snakeLength - 1; i >= 0; i--, j--)
-				snake[j] = snake[i];
-
-			snake->nodePos = after;
-
-			field[after.y][after.x] = head;
-			field[before.y][before.x] = body;
-
-			do {
-				foodPos = { rand() % FIELD_WIDTH,rand() % FIELD_LENGTH };
-			} while (field[foodPos.y][foodPos.x] != blank);
-			field[foodPos.y][foodPos.x] = food;
-
+			foodPos = { short(rand() % length),short(rand() % width) };
+			field[foodPos.y][foodPos.x] = element::food;
 			break;
-		case body:
-			cout << "YOU DIED!" << endl;
-			cout << "GAME OVER!" << endl;
-			system("pause");
-			return 0;
-		default:
-			fputs("unknown case was appeared, program exited, code: 1", stderr);
-			return 1;
 		}
 
-
-		Sleep(pauseTime);
-		pauseTime = 200;
-
-		cls();
-
+		PrintField();
+		std::this_thread::sleep_for(std::chrono::milliseconds(pauseTime));
+		clear_screen_no_sparkle(consoleHandle);
 	}
 	return 0;
 }
